60945e6070a86a7fc5e70e4aae3330fb140e8718
rafnas-p <rafnasrooni@gmail.com>
2024-10-18 16:03:01 +0530

    Updated

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-const express = require('express');
-const connectDB = require('./Config/db'); 
-const errorHandler=require('./middilewares/errorHandler')
-const userRoutes = require('./routes/userRoutes');
-const adminRouter=require('./routes/adminRouter')
-const cors=require('cors')
+const express = require("express");
+const connectDB = require("./Config/db");
+const errorHandler = require("./middilewares/errorHandler");
+const userRoutes = require("./routes/userRoutes");
+const adminRouter = require("./routes/adminRouter");
+const cors = require("cors");
 const app = express();
 const PORT = 3002;
 
-// Connect to the database
+
 connectDB();
 
-// Middleware to parse JSON requests
+
+
 app.use(express.json());
-app.use(cors({ origin: 'https://e-comerce-shoe.vercel.app',  
-  credentials: true,                
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  
-  allowedHeaders: ['Content-Type', 'Authorization']  
-}));
+app.use(
+  cors({
+    origin: "https://e-comerce-shoe.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use(errorHandler)
-// Import and use the user routes
-app.use('/users', userRoutes); 
 
-app.use('/admin',adminRouter);
-// Start the server
+app.options('*', cors());
+
+app.use(errorHandler);
+
+app.use("/users", userRoutes);
+
+app.use("/admin", adminRouter);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
